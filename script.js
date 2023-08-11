063e68f9454d9e0d791094e4d052b476dbdf564c
Fabrício Tofano Bersan <75699710+ftofano@users.noreply.github.com>
2023-08-11 20:01:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function tipo_dado()
-{
-    
+{    
 
-    let entrada = prompt("Digte algo .");
+    let entrada = prompt("Digte alguma coisa .");
 
-    if(!isNaN(entrada))
+    if(isNaN(entrada))
     {
         alert("Variavel do tipo string");
     }
@@ -14,21 +13,62 @@ function tipo_dado()
 
     }
 }
+function primo(){
+    let numero = parseInt(prompt("Digite um número: "));
+    let resposta = 0;
+    let divisores= 0;
+  
+    for(let count=1 ; count<=numero ; count++)
+     if(numero % count == 0)
+         divisores++;
+    
+    if(divisores==2)
+        alert("É primo");
+    else
+    alert("Não é primo");
+  }
 
 function par_impar()
 {
+    let numero = 0;
     do
     {
-        numero = prompt("Digite um número inteiro positivo!")
+        numero = prompt("Digite um número inteiro positivo!");
 
-    }while(isNaN(numero) && numero > 0)
+        if(isNaN(numero));
+        {
+            numero = parseInt(numero);            
+        } 
+
+    }while( numero < 0)
     if(numero % 2 == 0)
-    {
-        alert("O número digitado é par")
-
+    {        
+        alert("O número digitado é par");
     }
     else
     {
-        alert("O número digitado é impar")
+        alert("O número digitado é impar");
     }
 }
+
+function fatorial()
+{
+    let numero = prompt("Digite um número inteiro positivo.");
+    let resultado = 1;
+    if(numero == 0 || numero == 1)
+    {
+        alert("fatorial do número digitado é = "+resultado);
+    }
+    else
+    {
+        for(let i = 1; i <= numero; i++)
+        {
+            resultado *= i;
+        }
+        alert(resultado);
+    }
+    
+} 
+
+
+
